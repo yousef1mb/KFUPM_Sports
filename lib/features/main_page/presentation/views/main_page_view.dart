68d84654db669ea333672bfb9d1114a6d21c5941
hsbxkdie <120749128+hsbxkdie@users.noreply.github.com>
2024-11-12 16:04:34 +0300

    match_card layout

--- lib/features/main_page/presentation/views/main_page_view.dart
+++ lib/features/main_page/presentation/views/main_page_view.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
-import "package:kfupm_sports/features/main_page/presentation/widgets/bottom_navigation_bar.dart";
+import 'package:kfupm_sports/features/main_page/presentation/widgets/match_card.dart'; // Update this path based on your structure
 
-class MainPageView extends StatefulWidget {
-  @override
-  _MainPageViewState createState() => _MainPageViewState();
-}
-
-class _MainPageViewState extends State<MainPageView> {
-  int _currentIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-    // Here you can handle navigation to different pages based on the index
-  }
-
+class MainPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Main Page'),
+        title: Text('My Matches'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: Text(
-            'Selected Index: $_currentIndex'), // Placeholder for the body content
-      ),
-      bottomNavigationBar: BottomNavigationBarWidget(
-        currentIndex: _currentIndex,
-        onTap: _onItemTapped,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MatchCard(
+              sport: 'Football',
+              player: 'Ahmed Saad',
+              date: 'Monday 20 Oct',
+              location: 'Building 39',
+              playersJoined: '14/16',
+            ),
+            SizedBox(height: 16), // Spacing between cards
+            MatchCard(
+              sport: 'Volleyball',
+              player: 'Ahmed Saad',
+              date: 'Monday 20 Oct',
+              location: 'Building 39',
+              playersJoined: '14/16',
+            ),
+          ],
+        ),
       ),
     );
   }
